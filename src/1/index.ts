848b709fbba2008e08d449d2eaa300fcd2010816
christo <christopher.pkm@gmail.com>
2023-12-06 01:15:01 +0000

    part 1 of problem 1 completed, have 1 advent of code star. yaygit add .

---
+++ src/1/index.ts
@@ -0,0 +1,23 @@
+import { open as openFile } from "node:fs/promises";
+
+// part 1
+(async () => {
+    let firstLastNumbersOnEachLineAsDoubleDigits: number[] = [];
+    const absoluteFilePath = `${process.cwd()}/src/1/puzzleInput.txt`;
+    const file = await openFile(absoluteFilePath);
+
+    for await (const line of file.readLines()) {
+        const numberMatches = (line as string).match(/\d/g)!;
+        const firstNumber = numberMatches[0];
+        const secondNumber = numberMatches[numberMatches.length - 1];
+        const doubleDigitString: string = String(firstNumber) + String(secondNumber);
+        const doubleDigitNumeric: number = Number(doubleDigitString);
+        firstLastNumbersOnEachLineAsDoubleDigits.push(doubleDigitNumeric);
+    }
+
+    const sumOfDoubleDigits = firstLastNumbersOnEachLineAsDoubleDigits.reduce(
+        (prev, acum) => prev + acum,
+    );
+
+    console.log("sum of double digit numbers", sumOfDoubleDigits);
+})();
