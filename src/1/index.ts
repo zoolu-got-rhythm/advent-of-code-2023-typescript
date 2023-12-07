afdd7696953d58527198ec876d97b3d8bf34779d
christo <christopher.pkm@gmail.com>
2023-12-07 20:03:15 +0000

    getting there but not quite there yet

--- src/1/index.ts
+++ src/1/index.ts
@@ -1,13 +1,27 @@
 import { open as openFile } from "node:fs/promises";
+import { SourceTextModule } from "node:vm";
 
+type SearchType = "wordSearch" | "numberSearch";
 // part 1
-(async () => {
+const x = async (searchType: SearchType) => {
     let firstLastNumbersOnEachLineAsDoubleDigits: number[] = [];
     const absoluteFilePath = `${process.cwd()}/src/1/puzzleInputPart1.txt`;
     const file = await openFile(absoluteFilePath);
 
+    const numbersAsStrings: string[] = "zero one two three four five six seven eight nine".split(" ");
     for await (const line of file.readLines()) {
-        const numberMatches: string[] = (line as string).match(/\d/g)!;
+        let numberMatches: string[] = [];
+        if (searchType === "numberSearch") {
+            numberMatches = (line as string).match(/\d/g)!;
+        } else {
+            const numbersAsWordsMatches = (line as string).match(/one|two|three|four|five|six|seven|eight|nine/g);
+            if (numbersAsWordsMatches === null) {
+                continue;
+            }
+            numbersAsWordsMatches?.forEach((numberWord) => {
+                numberMatches.push(String(numbersAsStrings.indexOf(numberWord)));
+            });
+        }
         const firstNumber = numberMatches[0];
         const secondNumber = numberMatches[numberMatches.length - 1];
         const doubleDigitString: string = firstNumber + secondNumber; // string concatenation
@@ -15,9 +29,11 @@ import { open as openFile } from "node:fs/promises";
         firstLastNumbersOnEachLineAsDoubleDigits.push(doubleDigitNumeric);
     }
 
-    const sumOfDoubleDigits = firstLastNumbersOnEachLineAsDoubleDigits.reduce(
-        (prev, acum) => prev + acum,
-    );
+    const sumOfDoubleDigits = firstLastNumbersOnEachLineAsDoubleDigits.reduce((prev, acum) => prev + acum);
+    return sumOfDoubleDigits;
+};
 
-    console.log("sum of double digit numbers for part 1 =", sumOfDoubleDigits);
+(async () => {
+    console.log("part 1 answer =", await x("numberSearch"));
+    console.log("part 2 answer =", await x("wordSearch"));
 })();
