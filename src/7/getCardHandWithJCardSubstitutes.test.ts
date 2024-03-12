c6b6eca58c825eb7dc95362cf6e2c0706fb34f00
christo <christopher.pkm@gmail.com>
2024-03-12 00:53:04 +0000

    removed hello world unit test

--- src/7/getCardHandWithJCardSubstitutes.test.ts
+++ src/7/getCardHandWithJCardSubstitutes.test.ts
@@ -1,9 +1,5 @@
 import { getCardHandWithJCardSubstitutes } from "./index";
 
-test("3 + 2 === 5", () => {
-    expect(3 + 2).toBe(5);
-});
-
 test("32T3K === 32T3K", () => {
     expect(getCardHandWithJCardSubstitutes("32T3K")).toBe("32T3K");
 });
